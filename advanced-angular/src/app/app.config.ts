421e6be9caefbfbe9a9f3206f8c49be098d453d8
sebmat003 <sebmat003@utp.edu.pl>
2024-12-02 16:17:29 +0100

    angular 19 init

---
+++ advanced-angular/src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
+};
